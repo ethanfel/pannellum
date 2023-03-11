0e115f8595a1c5a9b203b179c4e709ec573883d1
ethanfel <ethan.fel@ts-pc.fr>
2023-03-11 12:15:01 +0100

    Update pano.sh

--- utils/multires/pano.sh
+++ utils/multires/pano.sh
@@ -5,7 +5,7 @@ for file in *.jpg; do
   # Set the output filename to the input filename with "_pano.png" appended
   output=$(echo $file | sed 's/\.jpg$/_pano.png/')
   # Run the Docker container and pass in the current file and output filename as arguments
-  sudo docker run -it -v $PWD:/data generate-panorama --png -f 1080 -H 180 -V 180 -F 120 --output /data/$output /data/$file
+  sudo docker run -it -rm -v $PWD:/data generate-panorama --png -f 1080 -H 180 -V 180 -F 120 --output /data/$output /data/$file
 done
 
 # Remove the Docker container with the ancestor image named 'generate-panorama'
